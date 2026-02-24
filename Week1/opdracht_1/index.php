3e3371e2d5ccdba4d9db96dac8b6e0b62f636b6b
ErayLale <9027890@student.zadkine.nl>
2026-02-24 14:11:21 +0100

    Initial commit

---
+++ Week1/opdracht_1/index.php
@@ -0,0 +1,19 @@
+<?php
+$naam = "Eray";
+$age = 17;
+$taal = "CSS";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> Mijn naam is <?= $naam ?></p>
+    <p> Mijn leeftijd is <?= $age ?> jaar</p>
+    <p> Mijn favoriete programmeertaal is <?= $taal ?></p>
+</body>
+</html>
