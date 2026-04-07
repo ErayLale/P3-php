fa72bf0ecf0cb3cd1e0210977cb9a2b3c17d25e1
ErayLale <9027890@student.zadkine.nl>
2026-04-07 14:27:06 +0200

    Update

--- Week5/opdracht_13/validatie_cijfer.php
+++ Week5/opdracht_13/validatie_cijfer.php
@@ -1,3 +1,19 @@
 <?php
-
+$aantal1 = "Aantal workouts:";   
+$aantal2 = "aantal minuten gesport:";      
+$aantal3 = "aantal glazen water:";        
+$aantal4 = 5;
+$aantal5 = 30;
+$aantal6 = 8;
+is_numeric($aantal4);
+is_numeric($aantal5);
+is_numeric($aantal6);
+echo "$aantal1 " . $aantal4 . "<br>";
+echo "$aantal2 " . $aantal5 . "<br>";
+echo "$aantal3 " . $aantal6 . "<br>";
+if (is_numeric($aantal4) && is_numeric($aantal5) && is_numeric($aantal6)) {
+    echo "Correct ingevoerd.";
+} else {
+    echo "Voer een geldig getal in.";
+}
 ?>
